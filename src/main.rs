ec0f1f8c48ffde8d1d32eb134914e4445d5fa53c
g4lvanix <elia@dl1ye.com>
2019-03-04 18:18:32 +0800

    updated license to GPLv3

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,36 @@ extern crate rustdw;
 use rustdw::*;
 
 extern crate clap;
-use clap::{Arg, App};
+use clap::{Arg, App, crate_version};
 
 fn main() {
     let matches = App::new("rustdw")
-               .version("0.2")
+               .version(crate_version!())
+               .long_version(concat!(crate_version!(), "
+
+    Copyright (C) 2019 Yuma Ritterbusch
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>"))
                .author("g4lvanix <elia@dl1ye.com>")
-               .about("Create diceware passwords and numeric pins")
+               .long_about("
+    Create diceware passwords and numeric pins
+
+    Copyright (C) 2019 Yuma Ritterbusch 
+    This program comes with ABSOLUTELY NO WARRANTY.
+    This is free software, and you are welcome to redistribute it
+    under certain conditions; see `rustdw --version` for details.
+")
                     .arg(Arg::with_name("file")
                          .short("f")
                          .help("Name of external diceware wordlist file")
